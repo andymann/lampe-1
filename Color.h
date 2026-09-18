e3b7be90ec1c3b38c4dfe48ec3116c567b08624b
Andy Fischer <info@Andyland.info>
2026-09-18 20:18:33 +0200

    Create Color.h

---
+++ Color.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Color { uint8_t r, g, b, w; };
+
+const Color COLOR_RED    = {255, 0,   0,   0};
+const Color COLOR_GREEN  = {0,   255, 0,   0};
+const Color COLOR_BLUE   = {0,   0,   255, 0};
+const Color COLOR_WHITE  = {0,   0,   0,   255};
+const Color COLOR_ORA    = {255, 134, 0,   0};
+const Color COLOR_VIO    = {255, 0,   255, 0};
+const Color COLOR_YELLOW = {255, 255, 0,   0};
